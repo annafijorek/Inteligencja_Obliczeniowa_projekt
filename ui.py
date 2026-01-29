6b3271d22b8be4143c14a1d6d8b3e9eaadf19735
Anna Fijorek <ania.j.fk@gmail.com>
2026-01-29 15:25:29 +0100

    changes in yolo script, changes in frontend

--- ui.py
+++ ui.py
@@ -1,59 +1,103 @@
 import customtkinter as ctk
 import cv2
 from PIL import Image, ImageTk
-from yolo_script import pilates_logic
+from yolo_script import logic
+
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
 
 
-class PilatesApp(ctk.CTk):
+class Workout_App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("Workout Assistant")
         self.geometry("1200x800")
 
-        self.logic = pilates_logic()
+        self.logic = logic()
+        self.is_running=False
+        self.exercise_buttons = {}
 
         self.cap = cv2.VideoCapture(0)
-        # self.cap = cv2.VideoCapture("video1.mp4") #nagranie z pliku
+        #self.cap = cv2.VideoCapture("resources/jumpingjack.mp4") #nagranie z pliku
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
         self.sidebar_frame = ctk.CTkFrame(self, width=200, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(5, weight=1)
+        self.sidebar_frame.grid_rowconfigure(8, weight=0)
 
-        self.logo_label = ctk.CTkLabel(self.sidebar_frame, text="Pilates AI", font=ctk.CTkFont(size=24, weight="bold"))
-        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        #zrodlo obrazu
+        self.source_label = ctk.CTkLabel(self.sidebar_frame, text="Zrodlo obrazu:", font=ctk.CTkFont(weight="bold"))
+        self.source_label.grid(row=0, column=0, padx=20, pady=(10, 5))
 
-        self.btn_plank = self.create_pink_button("Plank", "Plank", 1)
-        self.btn_warrior = self.create_pink_button("Warrior II", "Warrior II", 2)
-        self.btn_bird = self.create_pink_button("Bird Dog", "Bird Dog", 3)
+        self.source_buttons = {}
+        self.btn_cam = ctk.CTkButton(self.sidebar_frame, text="Kamera",
+                                 command=lambda: self.change_source(0, "cam"),
+                                 fg_color="#D81B60", hover_color="#AD1457", height=50, font=ctk.CTkFont(size=16))
+        self.btn_cam.grid(row=1, column=0, padx=20, pady=5)
+        self.source_buttons["cam"] = self.btn_cam
+
+        self.btn_plank_vid = ctk.CTkButton(self.sidebar_frame, text="Wideo: Plank",
+                                           command=lambda: self.change_source("resources/plank.mp4", "plank"),
+                                           fg_color="#D81B60", hover_color="#AD1457", height=50,
+                                           font=ctk.CTkFont(size=16))
+        self.btn_plank_vid.grid(row=2, column=0, padx=20, pady=5, sticky="ew")
+        self.source_buttons["plank"] = self.btn_plank_vid
+
+        self.btn_jump_vid = ctk.CTkButton(self.sidebar_frame, text="Wideo: Pajacyki",
+                                          command=lambda: self.change_source("resources/jumpingjack.mp4", "jump"),
+                                          fg_color="#D81B60", hover_color="#AD1457", height=50,
+                                          font=ctk.CTkFont(size=16))
+        self.btn_jump_vid.grid(row=3, column=0, padx=20, pady=5, sticky="ew")
+        self.source_buttons["jump"] = self.btn_jump_vid
+
+        #wybor cwiczenia
+        self.source_label = ctk.CTkLabel(self.sidebar_frame, text="Wybor cwiczenia:", font=ctk.CTkFont(weight="bold"))
+        self.source_label.grid(row=4, column=0, padx=20, pady=(10, 5))
+
+        self.btn_jump_vid.grid(row=3, column=0, padx=20, pady=5)
+        self.create_button("Plank", "Plank", 5)
+        self.create_button("Pajacyki", "JumpingJack", 6)
+
+        self.btn_reset = ctk.CTkButton(self.sidebar_frame, text="Resetuj Wynik",
+                                       command=self.reset_stats,fg_color="transparent",border_width=2, text_color="#FF69B4",
+                                      border_color="#FF69B4", hover_color="#333333")
+        self.btn_reset.grid(row=7, column=0, padx=20, pady=(20, 10), sticky="ew")
 
         self.btn_exit = ctk.CTkButton(self.sidebar_frame, text="Zakończ", command=self.close_app,
                                       fg_color="transparent", border_width=2, text_color="#FF69B4",
                                       border_color="#FF69B4", hover_color="#333333")
-        self.btn_exit.grid(row=6, column=0, padx=20, pady=20)
+        self.btn_exit.grid(row=8, column=0, padx=20,pady=10, sticky="ew")
 
         self.main_frame = ctk.CTkFrame(self, fg_color="transparent")
         self.main_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
 
         #feedback
-        self.feedback_label = ctk.CTkLabel(self.main_frame, text="Wybierz ćwiczenie...",
+        self.feedback_label = ctk.CTkLabel(self.main_frame, text="Wybierz cwiczenie",
                                            font=ctk.CTkFont(size=30, weight="bold"),
                                            text_color="#FF69B4")
-        self.feedback_label.pack(pady=(0, 20))
+        self.feedback_label.pack(pady=(20, 20))
 
         self.video_label = ctk.CTkLabel(self.main_frame, text="")
         self.video_label.pack(expand=True, fill="both")
-
         #wywolanie petli
         self.update_frame()
 
-    def create_pink_button(self, text, exercise_name, row):
+    def change_source(self, source,btn_key):
+        if self.cap is not None:
+            self.cap.release()
+        self.cap = cv2.VideoCapture(source)
+        for key, btn in self.source_buttons.items():
+            if key == btn_key:
+                btn.configure(fg_color="#880E4F")
+            else:
+                btn.configure(fg_color="#D81B60")
+        print(f"Zmieniono zrodlo na: {source}")
+
+    def create_button(self, text, exercise_name, row):
         btn = ctk.CTkButton(self.sidebar_frame, text=text,
                             command=lambda: self.change_exercise(exercise_name),
                             fg_color="#D81B60",
@@ -61,13 +105,29 @@ class PilatesApp(ctk.CTk):
                             height=50,
                             font=ctk.CTkFont(size=16))
         btn.grid(row=row, column=0, padx=20, pady=10, sticky="ew")
+        self.exercise_buttons[exercise_name] = btn
         return btn
+
+    def reset_stats(self):
+        self.logic.reps = 0
+        self.logic.counter = 0
+        self.logic.start_time = None
+        if hasattr(self.logic, 'pose'):
+            self.logic.pose = False
 
     def change_exercise(self, exercise_name):
         self.logic.current_exercise = exercise_name
         print(f"Zmieniono cwiczenie na: {exercise_name}")
-
-        self.feedback_label.configure(text=f"Tryb: {exercise_name}", text_color="#FF69B4")
+        self.reset_stats()
+        self.is_running = True
+        active_color = "#880E4F"
+        default_color = "#D81B60"
+        for name, btn in self.exercise_buttons.items():
+            if name == exercise_name:
+                btn.configure(fg_color=active_color)
+            else:
+                btn.configure(fg_color=default_color)
+        self.feedback_label.configure(text="")
 
     def close_app(self):
         self.cap.release()
@@ -75,6 +135,10 @@ class PilatesApp(ctk.CTk):
 
     def update_frame(self):
         success, frame = self.cap.read()
+
+        if not self.is_running:
+            self.after(30,self.update_frame)
+            return
 
         if not success:
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
@@ -85,6 +149,25 @@ class PilatesApp(ctk.CTk):
 
         try:
             annotated_frame, msg, color_hex = self.logic.video_check(frame)
+            self.feedback_label.configure(text=msg, text_color=color_hex)
+            # stale wymiary
+            # frame = cv2.resize(annotated_frame, (800, 600))
+            # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # img_pil = Image.fromarray(frame_rgb)
+            # img_tk = ctk.CTkImage(light_image=img_pil, dark_image=img_pil, size=(800, 600))
+
+            # wymiary obliczane
+            h, w, _ = annotated_frame.shape
+            target_width = 800
+            aspect_ratio = w / h
+            target_height = int(target_width / aspect_ratio)
+            img_rgb = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+            img_pil = Image.fromarray(img_rgb)
+            img_tk = ctk.CTkImage(light_image=img_pil, dark_image=img_pil, size=(target_width, target_height))
+
+            self.video_label.configure(image=img_tk)
+            self.video_label.image = img_tk
+
         except Exception as e:
             print(f"Blad w logice: {e}")
             annotated_frame = frame
@@ -92,21 +175,9 @@ class PilatesApp(ctk.CTk):
             color_hex = "#FF0000"
 
 
-        if self.feedback_label.cget("text") != msg:
-            self.feedback_label.configure(text=msg, text_color=color_hex)
-
-
-        frame = cv2.resize(annotated_frame, (800, 600))
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img_pil = Image.fromarray(frame_rgb)
-        img_tk = ctk.CTkImage(light_image=img_pil, dark_image=img_pil, size=(800, 600))
-
-        self.video_label.configure(image=img_tk)
-        self.video_label.image = img_tk
-
         self.after(10, self.update_frame)
 
 
 if __name__ == "__main__":
-    app = PilatesApp()
+    app = Workout_App()
     app.mainloop()
--- yolo_script.py
+++ yolo_script.py
@@ -1,12 +1,25 @@
 import cv2
 import numpy as np
+import time
+
+from sympy import false
 from ultralytics import YOLO
 
-class pilates_logic:
+class logic:
 
     def __init__(self):
-        self.model = YOLO('yolo11n-pose.pt')
+        self.model = YOLO('yolo11s-pose.pt')
+        #yolo11n-pose.pt- szybsza ale slabsza wersja yolo
         self.current_exercise="Plank"
+
+        self.start_time=None
+        self.reps=0
+        self.pose=False
+        self.stage="down"
+        self.time_limit = 1.5
+        #reps- powtorzenia
+        #pose-pozycja docelowa np noga w gorze
+
 
     #liczenie kata
     def calculate_angle(self,a, b, c):
@@ -24,20 +37,25 @@ class pilates_logic:
         return angle
 
 
-    def check_person(self,points):
+    def check_person(self,points, confidences):
         critical_points=[5,6,7,8,9,10,11,12,13,14,15,16]
         for i in critical_points:
-            if points[i][0]==0 or points[i][1]==0:
+            if points[i][0]==0 or points[i][1]==0 or confidences[i]<0.25:
                 return False
-            return True
+        return True
 
     def video_check(self,frame):
-        results = self.model(frame, conf=0.5, device='cpu',verbose=False) #verbose- logi ai
+        alpha=1.3
+        beta=40
+        ai_frame=cv2.convertScaleAbs(frame,alpha=1.1,beta=0)
+
+
+        results = self.model(ai_frame, conf=0.3, device='cpu',verbose=False) #verbose- logi ai
         # device=0 oznacza użycie karty graficznej NVIDIA
         # results = model(frame, conf=0.5, device=0)
         msg = ""
         color = "#FFFFFF"
-        annotated_frame=frame   #ustawienie domyslne, jesli nie znajdzie osoby
+        annotated_frame=frame.copy()   #ustawienie domyslne, jesli nie znajdzie osoby
 
 
         if results and results[0].keypoints is not None and len(results[0].keypoints.xy) > 0 :
@@ -45,15 +63,19 @@ class pilates_logic:
             # pobranie punktow x i y dla pierwwszej rozpoznanej osoby
             # YOLO keypoints: 11-lewe biodro, 13-lewe kolano, 15-lewa kostka
             points = r.keypoints.xy[0].cpu().numpy()
+
+            confidence=r.keypoints.conf[0].cpu().numpy()
             annotated_frame = r.plot()
             if len(points) > 0 and np.any(points):
-                full_body = self.check_person(points)
+                full_body = self.check_person(points,confidence)
                 if not full_body:
                     msg = "Oddal sie od kamery"
                     color = "#FF0000"
+                    self.start_time=None
                     return annotated_frame, msg, color
 
                 try:
+                    nose = points[0]
                     l_shoulder, l_elbow, l_wrist = points[5], points[7], points[9]
                     l_hip, l_knee, l_ankle = points[11], points[13], points[15]
 
@@ -71,7 +93,8 @@ class pilates_logic:
                         ("R_shoulder", r_hip, r_shoulder, r_elbow),
 
                         ("L_body", l_shoulder, l_hip, l_ankle),
-                        ("R_body", r_shoulder, r_hip, r_ankle)
+                        ("R_body", r_shoulder, r_hip, r_ankle),
+
                     ]
 
                     # zapisywanie wynikow
@@ -82,8 +105,8 @@ class pilates_logic:
                             ang = self.calculate_angle(pA, pW, pC)
                             calculated_angles[label] = ang
 
-                            # wizualizacja
-                            cv2.putText(frame, f"{label}: {int(ang)}", tuple(pW.astype(int)),
+                            # wizualizacja annotated_frame-wyswietlanie katow, frame- bez katow
+                            cv2.putText(annotated_frame, f"{label}: {int(ang)}", tuple(pW.astype(int)),
                                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
                     l_knee_angle = calculated_angles.get("L_knee", 0)
@@ -94,47 +117,83 @@ class pilates_logic:
                     r_shoulder_angle = calculated_angles.get("R_shoulder", 0)
                     l_hip_angle = calculated_angles.get("L_hip", 0)
                     r_hip_angle = calculated_angles.get("R_hip", 0)
+                    l_elbow_angle=calculated_angles.get("L_elbow", 0)
+                    r_elbow_angle=calculated_angles.get("R_elbow",0)
 
                     if self.current_exercise == "Plank":
-                        is_straight = (l_body_angle > 165) or (r_body_angle > 165)
-                        if is_straight:
-                            msg = "Tak trzymaj!"
+                        is_straight = (190>l_body_angle > 165) or (190>r_body_angle > 165)
+                        #arms_straight = (l_elbow_angle > 160) or (r_elbow_angle > 160)
+                        arms_90 = (70 < l_elbow_angle < 110) or (70 < r_elbow_angle < 110)
+                        is_horizontal = abs(l_shoulder[1] - l_hip[1]) < 100 or abs(r_shoulder[1] - r_hip[1]) < 100
+                        not_floor = (l_hip[1] < l_ankle[1] - 20) or (r_hip[1] < r_ankle[1] - 20)
+                        if is_straight and arms_90 and is_horizontal and not_floor:
+                            if self.start_time is None:
+                                self.start_time=time.time()
+                            workout_time=int(time.time() - self.start_time)
+                            msg = f"Tak trzymaj! Czas: {workout_time}s"
                             color = "#00FF00"
                         elif l_body_angle > 0 or r_body_angle > 0:
                             msg = "Popraw pozycje"
                             color = "#FF0000"
+                            self.start_time=None
                         else:
                             msg = "Nikogo nie widze :(("
-                    elif self.current_exercise == "WarriorII":
-                        # prawa lub lewa nowa jest zgieta 90stopni druga musi byc prosta
-                        # trzeba poprawic aby tylko jedna opcje mogla byc
-                        left_leg = (80 < l_knee_angle < 110) and (r_knee_angle > 160)
-                        right_leg = (80 < r_knee_angle < 110) and (l_knee_angle > 160)
-                        arms_position = (l_shoulder_angle > 80) and (r_shoulder_angle > 80)
+                            self.start_time=None
+                    elif self.current_exercise == "JumpingJack":
+                        # msg=""
+                        # color=""
 
-                        if (left_leg or right_leg) and arms_position:
-                            msg = "Tak trzymaj!"
+                        hands_up = l_wrist[1] < nose[1] and r_wrist[1] < nose[1]
+                        shoulder_dist = np.linalg.norm(l_shoulder - r_shoulder)
+                        feet_dist = np.linalg.norm(l_ankle - r_ankle)
+                        feet_wide = feet_dist > (shoulder_dist * 1.5)
+                        current_time = time.time()
+                        if self.stage == "down" and self.reps > 0:
+                            if current_time - getattr(self, 'last_rep_time', current_time) > 3.0:
+                                self.reps = 0
+                                msg = f"Wynik: {self.reps}"
+                                color = "#FF0000"
+                        if hands_up and feet_wide:
+                            if self.stage == "down":
+                                self.stage = "up"
+                                self.rep_start_time = current_time
+                            msg = f"Wynik: {self.reps}"
                             color = "#00FF00"
-                        elif (left_leg or right_leg) and not arms_position:
-                            msg = "Popraw pozycje rak"
-                            color = "#FF0000"
-                        else:
-                            msg = "Popraw pozycje nog"
-                            color = "#FF0000"
-                        # tu jeszcze wiecej warunkow???
+                        elif not hands_up and not feet_wide:
+                            if self.stage == "up":
+                                duration = current_time - self.rep_start_time
+                                if duration <= self.time_limit:
+                                    self.reps += 1
+                                else:
+                                    self.reps = 0
 
-                    elif self.current_exercise == "Bird Dog":
-                        leg_extended = (l_hip_angle > 160) or (r_hip_angle > 160)
-
-                        if leg_extended:
-                            msg = "Tak trzymaj!"
-                            color = "#00FF00"
+                                self.stage = "down"
+                                self.rep_start_time = None
+                                msg = ""
+                                color = "#FF0000"
+                            else:
+                                if self.stage == "up" and (current_time - self.rep_start_time) > self.time_limit:
+                                    self.reps = 0
+                                    self.stage = "down"
+                                msg = ""
+                                color = "#FFFF00"
                         else:
-                            msg = "Popraw pozycje"
-                            color = "#FF0000"
+                            msg = "Powtorzenie wykonane niepoprawnie"
+                            color = "#FFFF00"
+
+
+
+                    else:
+                        msg="nie wybrano cwiczenia"
+                        color = "#FF0000"
 
                 except IndexError:
-                    pass
+                    msg="nie ma wszystkich punktow"
+                    color = "#FFFF00"
+                except Exception as e:
+                    print(f"Błąd: {e}")
+                    msg = "Blad"
+                    color = "#FFFF00"
 
             return annotated_frame, msg, color
 
